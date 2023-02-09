5149aef0e19b32ef35fa7ab80a2f3d8f5a8c11b7
Ali Hashimov <ali.hashimovv@gmail.com>
2023-02-09 20:00:31 +0400

    optional algorithm example added

---
+++ src/az/et/optional/PhoneBookCrawler.java
@@ -0,0 +1,55 @@
+package az.et.optional;
+
+import java.util.Optional;
+
+public class PhoneBookCrawler {
+
+    private final PhoneBook phoneBook;
+
+    public PhoneBookCrawler(PhoneBook phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public static void main(String[] args) {
+        PhoneBookCrawler p = new PhoneBookCrawler(new PhoneBook());
+        System.out.println(p.findPhoneNumberByNameOrNameByPhoneNumber("Raf de Giraf", "016/161619"));
+    }
+
+    public String findPhoneNumberByNameAndPunishIfNothingFound(String name){
+        return phoneBook.findPhoneNumberByName(name)
+                .orElseThrow(() -> new IllegalArgumentException("No phone number found"));
+    }
+
+    public String findPhoneNumberByNameAndPrintPhoneBookIfNothingFound(String name){
+        final StringBuilder result = new StringBuilder();
+        phoneBook.findPhoneNumberByName(name)
+                .ifPresentOrElse(phoneNumber->result.append(phoneNumber), //Java11
+                () -> System.out.println(phoneBook.getPhoneBookEntries()));
+        return result.toString();
+    }
+
+    public String findPhoneNumberByNameOrNameByPhoneNumber(String name, String phoneNumber) {
+        Optional<String> phoneNumberOptional = phoneBook.findPhoneNumberByName(name);
+        Optional<String> nameOptional = phoneBook.findNameByPhoneNumber(phoneNumber);
+
+        if(phoneNumberOptional.isPresent()) {
+            return phoneNumberOptional.get();
+        } else if(nameOptional.isPresent()) {
+            return nameOptional.get();
+        }
+        //return new ArrayList<>(phoneBook.getPhoneBookEntries().values()).get(0);
+
+        return phoneBook.findPhoneNumberByName(name)
+                .orElse(phoneBook.findNameByPhoneNumber(phoneNumber)
+                        .orElse(phoneBook.findPhoneNumberByName("Jos de Vos")
+                                .orElse(null)
+                        )
+                );
+
+    }
+
+    public PhoneBook getPhoneBook(){
+        return phoneBook;
+    }
+
+}
--- src/az/et/stream/Challenge.java
+++ src/az/et/stream/Challenge.java
@@ -29,4 +29,8 @@ public class Challenge {
                 })
                 .collect(Collectors.joining(","));
     }
+
+    public static class Test{
+
+    }
 }
